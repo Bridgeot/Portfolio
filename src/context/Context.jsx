25a4583a94c7edf4c223686f1266b7a865f64c0d
Bridgeot <bridgeot789@gmail.com>
2023-06-22 17:23:34 -0400

    wip: add context content

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client'
 
-const Context = () => {
-  return (
-    <div>Context</div>
-  )
+import { createContext, useState } from 'react'
+
+export const Context = createContext()
+
+export const ThemeProvider = ({children}) => {
+    const [mode, setMode] = useState('')
+
+    const toggle = () => {
+        setMode((prev) => (prev === 'dark' ? '' : 'dark'))
+    }
+
+    return (
+        <Context.Provider value={{ toggle, mode }}>
+            <div className={`theme ${mode}`}>{children}</div>
+        </Context.Provider>
+    )
 }
-
-export default Context
