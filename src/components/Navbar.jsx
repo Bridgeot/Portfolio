8600281ccc5ee86dbd141936d794b28cbdc1b944
Bridgeot <bridgeot789@gmail.com>
2023-06-22 17:15:49 -0400

    wip: add navbar component content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import styles from '@/styles/components/navbar.module.css'
+import Toggle from '@/components/Toggle'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <a href='#01'>01.</a>
+        </li>
+        <li className={styles.link}>
+          <a href='#02'>02.</a>
+        </li>
+        <li className={styles.link}>
+          <a href='#03'>03.</a>
+        </li>
+        <li className={styles.link}>
+          <a href='#04'>04.</a>
+        </li>
+        <li className={styles.link}>
+          <Toggle />
+        </li>
+      </ul>
+    </nav>
   )
 }
 
