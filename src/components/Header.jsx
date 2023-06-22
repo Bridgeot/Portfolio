521c6ce7cae3df5a5596deddc34b7b988d2b5d2c
Bridgeot <bridgeot789@gmail.com>
2023-06-22 17:14:58 -0400

    wip: add header component content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import styles from '@/styles/components/header.module.css'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <Link href='/'>
+        <h1 className={styles.heading}>
+          Bridget Gordon
+        </h1>
+      </Link>
+      <p className={styles.text}>
+        Front-End Developer / Full-Stack Developer / 
+        Adobe Digital Academy Scholar 2023
+      </p>
+    </header>
   )
 }
 
