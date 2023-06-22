e31dbf174670d27d2177b5565d8c93cbe3b35fac
Bridgeot <bridgeot789@gmail.com>
2023-06-22 17:22:22 -0400

    wip: add home page content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
 import styles from '@/styles/page.module.css'
+import Header from '@/components/Header'
+import About from '@/sections/About'
+import Experience from '@/sections/Experience'
+import Skills from '@/sections/Skills'
+import Work from '@/sections/Work'
+import Footer from '@/components/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className={styles.home}>
+      <div className={styles.container}>
+        <Header />
+      </div>
+      <div className={styles.container}>
+        <About />
+        <Experience />
+        <Skills />
+        <Work />
+        <Footer />
+      </div>
+    </section>
   )
 }
 
